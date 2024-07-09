e407093a1a17a8e906683c0e7c8751dea61bb0b7
Atharva Goliwar <atharva.goliwar23@gmail.com>
2024-07-09 17:21:44 +0530

    backend cors check

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ app.use(session({
 
 app.use(express.json({limit:"10mb"}))
 // app.use(cors({origin:"http://localhost:5173",credentials:true}))
-app.use(cors({origin:"https://surya-electronics-web-app.vercel.app/",credentials:true}))
+// app.use(cors({origin:"https://surya-electronics-web-app.vercel.app/",credentials:true}))
+app.use(cors())
 // app.use(cors({origin:"https://tvbfxxwf-5173.inc1.devtunnels.ms/",credentials:true}))
 
 

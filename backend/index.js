bddcd006a0d474fb88a17019c1ed4e77f91dfb47
Atharva Goliwar <atharva.goliwar23@gmail.com>
2024-07-10 15:22:35 +0530

    check db quit issue

--- backend/index.js
+++ backend/index.js
@@ -329,13 +329,13 @@ app.post('/logout',requireAuth, (req, res) => {
         secure: true
       });
       // Close the connection when done with it
-    db.end((err) => {
-        if (err) {
-          console.error('Error closing MySQL connection:', err);
-        } else {
-          console.log('MySQL connection closed');
-        }
-      });
+    // db.end((err) => {
+    //     if (err) {
+    //       console.error('Error closing MySQL connection:', err);
+    //     } else {
+    //       console.log('MySQL connection closed');
+    //     }
+    //   });
     // cookies.set("token",null,{httpOnly:true})
 
     res.json({ message: 'Logout successful' });
